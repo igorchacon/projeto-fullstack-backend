5ab0c3cbcbd75901df5ec2f57c9860bc0e9484bb
Igor Chacon <igorchacon@yahoo.com.br>
2021-04-27 22:58:07 -0300

    Preparando projeto

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { AddressInfo } from "net";
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+
+const server = app.listen(process.env.PORT || 3003, () => {
+    if (server) {
+      const address = server.address() as AddressInfo;
+      console.log(`Server is running in http://localhost:${address.port}`);
+    } else {
+      console.error(`Failure upon starting server.`);
+    }
+  });;
